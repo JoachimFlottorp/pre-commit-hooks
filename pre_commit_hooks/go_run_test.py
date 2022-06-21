00693f37e7f17fb5a8876f35bd94145886af0e79
Joachim Flottorp <joachim@flottorp.org>
2022-06-21 17:57:05 +0200

    Revert "feat: go fmt"
    This reverts commit d56c7d66b902ace39a74825b4985517713336bae.

--- pre_commit_hooks/go_run_test.py
+++ pre_commit_hooks/go_run_test.py
@@ -1,41 +1,36 @@
 from __future__ import annotations
 
+from typing import Sequence 
 import subprocess
-from dataclasses import dataclass, field
 
-@dataclass
-class Result:
-    okay: bool
-    reason: str = field(default="")
-
-def was_fail(src: bytes) -> Result:
+def was_fail(src: bytes) -> bool:
     split = src.split(b'\n')
     for line in split:
         if b'FAIL' in line:
-            return Result(False, line.decode('utf-8'))
-
-    return Result(True)
+            return True
+        
+    return False
 
 def test() -> bool:
+    # Expects to be in the project directory.
+
     proc = subprocess.run(["go", "test", "./...", "-count=1"], capture_output=True)
     stdout = proc.stdout
     stderr = proc.stderr
     if stdout is not None:
-        result = was_fail(stdout)
-        if result.okay is False:
-            print(result.reason)
-            return result.okay
-
+        if was_fail(stdout) is True:
+            print(stdout.decode('utf-8'))
+            return False
+        
     if stderr is not None:
-        result = was_fail(stderr)
-        if result.okay is False:
-            print(result.reason)
-            return result.okay
+        if was_fail(stderr) is True:
+            print(stderr.decode('utf-8'))
+            return False
 
     return True
-
-def main() -> int:
+    
+def main(args: Sequence[str] | None = None) -> int:
+    '''
+        Runs every test in the directory.
+    '''
     return 0 if test() == True else 1
-
-if __name__ == "__main__":
-    main()
